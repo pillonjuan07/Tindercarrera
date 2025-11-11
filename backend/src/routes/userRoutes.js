0fda2ba69262b03bb866b16d45c8add7ea6dd92e
Ulises Urquiza <ulisesurquiza2@gmail.com>
2025-11-10 22:11:22 -0300

    Revert "Prueeba del Backend que todo corra bien"

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrGetUser, getUserById } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.post('/login', createOrGetUser);
-router.get('/:id', getUserById);
-
-export default router;
